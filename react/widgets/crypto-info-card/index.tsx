fa32411c724963bf299429f1252658317c178ae1
codad5 <aniezeoformic@gmail.com>
2024-07-25 22:33:24 +0100

    chore: Add crypto-info-card widget

---
+++ react/widgets/crypto-info-card/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactRender from "../../helper-components/react-wrapper";
+import '@/styles/sass/crypto-info-card.scss'
+
+ReactRender(() => {
+  return (
+    <div className="ucp-crypto-info-card">
+             Hello this is crypto-info-card
+    </div>
+  )
+})
