dab110921331ffe3a86e813f87be4b4e62d26dcf
codad5 <aniezeoformic@gmail.com>
2024-07-25 22:30:54 +0100

    chore: Add donation box widgets

---
+++ react/widgets/donation-box-2/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactRender from "../../helper-components/react-wrapper";
+import '@/styles/sass/donation-box-2.scss'
+
+ReactRender(() => {
+  return (
+    <div className="ucp-donation-box-2">
+             Hello this is donation-box-2
+    </div>
+  )
+})
