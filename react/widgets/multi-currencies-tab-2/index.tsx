68d7631068222182de0522f8c6f03e1eaff8ba89
codad5 <aniezeoformic@gmail.com>
2024-07-25 22:39:15 +0100

    chore: Add new widgets for multi-currencies tab

---
+++ react/widgets/multi-currencies-tab-2/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactRender from "../../helper-components/react-wrapper";
+import '@/styles/sass/multi-currencies-tab-2.scss'
+
+ReactRender(() => {
+  return (
+    <div className="ucp-multi-currencies-tab-2">
+             Hello this is multi-currencies-tab-2
+    </div>
+  )
+})
---
+++ react/widgets/multi-currencies-tab/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactRender from "../../helper-components/react-wrapper";
+import '@/styles/sass/multi-currencies-tab.scss'
+
+ReactRender(() => {
+  return (
+    <div className="ucp-multi-currencies-tab">
+             Hello this is multi-currencies-tab
+    </div>
+  )
+})
