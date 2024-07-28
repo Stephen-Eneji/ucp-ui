3c5d83efa6e37c9e327be38c8d93f0f769561079
codad5 <aniezeoformic@gmail.com>
2024-07-28 13:24:22 +0100

    chore: Add price change percentage fields to CoinData interface and update crypto-price-table component

--- react/widgets/crypto-date-change-table/index.tsx
+++ react/widgets/crypto-date-change-table/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactRender from "../../helper-components/react-wrapper";
-import '@/styles/sass/crypto-price-table.scss'
-import { abbreviateNumber, levenshteinDistance } from "../../helper/helper";
+import "@/styles/sass/crypto-date-change-table.scss";
+import { levenshteinDistance, roundToSignificantFigures } from "../../helper/helper";
 import { CoinData } from "../../types";
 
 ReactRender(({ coins, settings }) => {
@@ -46,47 +46,39 @@ ReactRender(({ coins, settings }) => {
     setCoinList(filtered);
   };
 
-
   let width = settings.parent_width;
-  const isStyle2 = settings.card === 'card-002'
   // if width does not end with % or px then add px
-  if (!width.endsWith('%') &&!width.endsWith('px')) {
-    width += 'px';
+  if (!width.endsWith("%") && !width.endsWith("px")) {
+    width += "px";
   }
+
   return (
-    <div className="ucp-crypto-price-table" style={{ width: width }}>
-      <div className="ucp-crypto-price-table-main">
-        {isStyle2 && (
-          <input
-            type="text"
-            className="ucp-crypto-price-table-main-search ucp-crypto-search-input"
-            placeholder={`${settings.search_placeholder}`}
-            onChange={search}
-          />
-        )}
-        <table className={`ucp-crypto-price-table-main-table`}>
+    <div className="ucp-crypto-date-change-table" style={{ width: width }}>
+      <div className="ucp-crypto-date-change-table-main">
+        <input
+          type="text"
+          className="ucp-crypto-date-change-table-main-search ucp-crypto-search-input"
+          placeholder={`${settings.search_placeholder}`}
+          onChange={search}
+        />
+        <table className={`ucp-crypto-date-change-table-main-table`}>
           <thead>
             <tr>
               <th></th>
               <th>#Name</th>
               <th>Price </th>
               <th>24H Change</th>
-              {isStyle2 && (
-                <>
-                  <th>Market Cap</th>
-                  <th> Volume</th>
-                  <th> Supply</th>
-                </>
-              )}
+              <th>7D Change</th>
+              <th>30D Change</th>
             </tr>
           </thead>
           <tbody>
-            {coinList.slice(0, settings.count ?? 10).map((coin, index) => (
+            {coinList.map((coin, index) => (
               <tr
                 key={index}
                 data-table-row-coin={`${coin.name}---${coin.symbol}`}
               >
-                <td>{index}</td>
+                <td>{index + 1}</td>
                 <td>
                   <div className="crypto-price-table-name-info">
                     <div className="crypto-price-table-name-info-image">
@@ -108,15 +100,34 @@ ReactRender(({ coins, settings }) => {
                       coin.price_change_percentage_24h > 0 ? "up" : "down"
                     }`}
                   ></i>
-                  {coin.price_change_percentage_24h}
+                  {roundToSignificantFigures(coin.price_change_percentage_24h, 4)}
                 </td>
-                {isStyle2 && (
-                  <>
-                    <td>{abbreviateNumber(coin.market_cap)}</td>
-                    <td>{abbreviateNumber(coin.total_volume)}</td>
-                    <td>{abbreviateNumber(coin.circulating_supply)}</td>
-                  </>
-                )}
+                <td>
+                  <i
+                    className={`fa-solid fa-arrow-${
+                      coin.price_change_percentage_7d_in_currency ?? 0 > 0
+                        ? "up"
+                        : "down"
+                    }`}
+                  ></i>
+                  {roundToSignificantFigures(
+                    coin.price_change_percentage_7d_in_currency ?? 0,
+                    4
+                  )}
+                </td>
+                <td>
+                  <i
+                    className={`fa-solid fa-arrow-${
+                      coin.price_change_percentage_30d_in_currency ?? 0 > 0
+                        ? "up"
+                        : "down"
+                    }`}
+                  ></i>
+                  {roundToSignificantFigures(
+                    coin.price_change_percentage_30d_in_currency ?? 0,
+                    4
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -124,4 +135,4 @@ ReactRender(({ coins, settings }) => {
       </div>
     </div>
   );
-})
+});
